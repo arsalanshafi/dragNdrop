6d63fb6f914a2ff6b7de14e93282c77ee7f865a7
abuArsalan <arsalanshafi911@gmail.com>
2024-02-11 14:49:51 +0530

    adedd random colors

--- public/script.js
+++ public/script.js
@@ -1,5 +1,12 @@
 const shapes = [...document.querySelectorAll("li")];
 const canvas = document.querySelector("#canvas");
+const rect = canvas.getBoundingClientRect();
+const CANVAS_X = rect.x;
+const CANVAS_Y = rect.y;
+const OFFSET = 50;
+// console.log(rect);
+
+
 
 shapes.forEach(shape => {
     shape.addEventListener("dragend", e => {
@@ -10,16 +17,36 @@ shapes.forEach(shape => {
 })
 
 function paste(ele,x,y){
+
+    if(x < rect.x || x >rect.right || y < rect.y || y > rect.bottom) return;
+
     let field = document.createElementNS('http://www.w3.org/2000/svg',"svg");
     field.setAttribute("width",100);
     field.setAttribute("height",100);
-    let shape = document.createElementNS('http://www.w3.org/2000/svg',`${ele}`);
-    shape.setAttribute("width",100);
-    shape.setAttribute("height",100);
-    shape.setAttribute("fill","rgb(0,255,191");
     field.style.position = "absolute";
-    field.style.left = "50px";
+    let figure = document.createElementNS('http://www.w3.org/2000/svg',`${ele}`);
 
-    field.appendChild(shape);
+    switch (ele){
+        case "rect":
+            figure.setAttribute("width",100);
+            figure.setAttribute("height",100);
+            break;
+        case "circle":
+            figure.setAttribute("cx",50);
+            figure.setAttribute("cy",50);
+            figure.setAttribute("r",50);
+            break;
+        case "polygon":
+            figure.setAttribute("points","0,100 50,0 100,100");
+            break;
+        default:
+            console.log("default");
+            break;
+    }
+    figure.setAttribute("fill",`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`);
+    field.style.left = `${(x-CANVAS_X)-OFFSET}px`;
+    field.style.top = `${(y-CANVAS_Y)-OFFSET}px`;
+    field.appendChild(figure);
     canvas.appendChild(field);
 }
+
